9a47697cb9fecfd7a41639198895f49938d1bf73
Mohammed Kassem <moka_is@hotmail.com>
2016-10-27 15:57:53 +0300

    create solution files

---
+++ module4-solution/src/menuapp/menudata.service.js
@@ -0,0 +1,49 @@
+(function () {
+'use strict';
+
+angular.module('data')
+.service('MenuDataService', MenuDataService);
+
+MenuDataService.$inject = ['$http', 'ApiBasePath']
+function MenuDataService($http, ApiBasePath) {
+  var service = this;
+
+  // Simulates call to server
+  // Returns a promise, NOT items array directly
+  service.getAllCategories = function () {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/categories.json")
+    });
+    // .then(function (response) {
+    //   var menuItems = response.data.menu_items;
+    //   var foundItems= menuItems.filter(function (el) {
+    //     return el.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+    //   });
+    //
+    //   return foundItems;
+    // });
+
+    return response;
+  };
+
+  service.getItemsForCategory = function (categoryShortName) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+      data :{category : categoryShortName}
+    });
+    // .then(function (response) {
+    //   var menuItems = response.data.menu_items;
+    //   var foundItems= menuItems.filter(function (el) {
+    //     return el.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+    //   });
+    //
+    //   return foundItems;
+    // });
+
+    return response;
+  };
+}
+
+})();
---
+++ module4-solution/src/routes.js
@@ -0,0 +1,42 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+
+  // categories page
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/categories.template.html',
+    controller: 'CategoriesController as categories',
+    // resolve: {
+    //   items: ['ShoppingListService', function (ShoppingListService) {
+    //     return ShoppingListService.getItems();
+    //   }]
+    // }
+  })
+  //
+  // .state('mainList.itemDetail', {
+  //   url: '/item-detail/{itemId}',
+  //   templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
+  //   controller: "ItemDetailController as itemDetail"
+  // });
+
+}
+
+})();
