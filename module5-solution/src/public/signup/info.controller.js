08013e5e72c6361cd62c9ba6ee7b9a30761cab3c
Mohammed Kassem <moka_is@hotmail.com>
2016-11-06 15:28:25 +0300

    final

--- module5-solution/src/public/signup/info.controller.js
+++ module5-solution/src/public/signup/info.controller.js
@@ -8,12 +8,5 @@
   function InfoController(SignupSaveService) {
     var infoCtrl = this;
     infoCtrl.user = SignupSaveService.get();
-
-    console.log("infoCtrl.user: "+infoCtrl.user);
-    //save sign up info into shared service.
-    infoCtrl.submit = function () {
-      SignupSaveService.save(signupCtrl.user);
-    };
   }
-
 })();
--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -4,15 +4,31 @@
   angular.module('common')
   .controller('SignupController', SignupController);
 
-  SignupController.$inject = ["SignupSaveService"];
-  function SignupController(SignupSaveService) {
+  SignupController.$inject = ["SignupSaveService", "MenuService", "$scope"];
+  function SignupController(SignupSaveService, MenuService, $scope) {
     var signupCtrl = this;
     signupCtrl.user = {};
 
     //save sign up info into shared service.
     signupCtrl.submit = function () {
-      SignupSaveService.save(signupCtrl.user);
+      //check if menu item id is valid
+      var promise = MenuService.getMenuItem(signupCtrl.user.menuItemId);
+
+      promise.then(function (response) {
+        //set form validaty to true.
+        $scope.signupForm.$setValidity("menuItemId", true, $scope.signupForm);
+        signupCtrl.user.menuItem = response;
+        //Save user info to sign up service.
+        SignupSaveService.save(signupCtrl.user);
+        signupCtrl.infoSaved = true;
+      })
+      .catch(function (error) {
+        //set form validaty to false.
+        $scope.signupForm.$setValidity("menuItemId", false, $scope.signupForm);
+        $scope.signupForm.menuItemId.$error.serverMessage = 'No such menu number exists';
+      });
     };
+
   }
 
 })();
